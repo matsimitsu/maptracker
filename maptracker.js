91522f99ba35b3b7d13c86eeed678c74a48abc78
Robert Beekman <robert@matsimitsu.nl>
2011-06-13 14:45:48 +0200

    fixed remoteaddress

--- maptracker.js
+++ maptracker.js
@@ -66,7 +66,7 @@ app.get('/map', function(req, res) {
 app.get('/pixel.gif', function(req, res) {
   res.writeHead(200, pixelHeaders);
   res.end(pixel);
-  remote_ip = req.remoteAddress
+  remote_ip = req.connection.remoteAddress
   site_name = 'http://matsimitsu.com'
   cities.lookup(remote_ip, function(err, data) {
       if (err) {
