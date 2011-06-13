b9ddfa6543d9f6b130ae1a46d4566fae88430110
Robert Beekman <robert@matsimitsu.nl>
2011-06-13 14:16:52 +0200

    added actual pixel, removed stuff i dont need

--- maptracker.js
+++ maptracker.js
@@ -13,11 +13,20 @@ var http = require('http')
   , City = geoip.City
   , cities = new City(__dirname + '/GeoLiteCity.dat')
 
-  , port = process.argv.length >= 4 && process.argv[4] || process.env.PORT || process.env.POLLA_PORT || 8080
-  , host = process.argv.length >= 3 && process.argv[3] || process.env.HOST || process.env.POLLA_HOST || 'localhost'
+  , port = process.argv.length >= 3 && process.argv[3] || process.env.PORT || process.env.POLLA_PORT || 8080
+  , host = process.argv.length >= 2 && process.argv[2] || process.env.HOST || process.env.POLLA_HOST || 'localhost'
 
   , wsport = process.env.WSPORT || + port + 111
   , wshost = process.env.WSHOST || host
+
+  , fs = require('fs')
+  , pixel = fs.readFileSync(__dirname + '/pixel.gif')
+  , pixelHeaders = {
+    'Cache-Control': 'private, no-cache, proxy-revalidate',
+    'Content-Type': 'image/gif',
+    'Content-Disposition': 'inline',
+    'Content-Length': pixel.length
+  }
 
 // configuration
 var app = express.createServer()
@@ -54,8 +63,9 @@ app.get('/map', function(req, res) {
   res.render('map', { layout: false, locals: { title: 'maptrack', wshost: wshost, wsport: wsport } })
 })
 
-app.get('/pixel.jpg', function(req, res) {
-  res.send(200, {}, {});
+app.get('/pixel.gif', function(req, res) {
+  res.writeHead(200, pixelHeaders);
+  res.end(pixel);
   remote_ip = '92.254.85.252' // req.remoteAddress
   site_name = 'http://localhost'
   cities.lookup(remote_ip, function(err, data) {
@@ -192,79 +202,6 @@ var world = {
   }
 }
 
-// tail -n 1000 -f filename
-
-//var tail = spawn('tail', ['-n', '1000', '-f', filename])
-//
-//tail.stdout.on('data', function (data) {
-//  var dataStr = data.toString()
-//
-//  dataStr.replace(/([^\n]+)\n/g, function(m, line) {
-//    // proxy tail
-//    console.log(line)
-//
-//    var ipsArray = line.match(/(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/gm) || []
-//      , ips = {}
-//      , ipsplit = []
-//
-//    ipsArray.forEach(function(ip) {
-//      // no version numbers
-//      ipsplit = ip.split('.')
-//      for (var i = 0, c = 0; i < ipsplit.length; i++) {
-//        if (ipsplit[i] < 10) c++
-//        if (c === 3) return
-//      }
-//
-//      ips[ip] = { name: ip, line: htmlspecialchars(line), lastActivity: Date.now() }
-//    })
-//
-//    for (var ip in ips) {
-//      // Return an object of city information
-//      // {
-//      //  "country_code":"US",
-//      //  "country_code3":"USA",
-//      //  "country_name":"United States",
-//      //  "continet_code":"NA",
-//      //  "region":"CA",
-//      //  "city":"Mountain View",
-//      //  "postal_code":"94043",
-//      //  "latitude":37.41919999999999,
-//      //  "longitude":-122.0574,
-//      //  "dma_code":807,
-//      //  "metro_code":807,
-//      //  "area_code":650
-//      //  }
-//      if (typeof ips[ip].city === 'undefined') {
-//        city = geoip.City.record_by_addr(cities, ip)
-//        if (city) {
-//          ips[ip].city = city
-//          ips[ip].lat = city.latitude
-//          ips[ip].lng = city.longitude
-//
-//          world.messageCount++
-//          world.users[ip] = ips[ip]
-//
-//          world.sendUpdate(ips[ip])
-//        } else {
-//          ips[ip].city = {}
-//          ips[ip].lat = 0
-//          ips[ip].lng = 0
-//          console.log('Could not grab location for', ip, ' - ', city)
-//        }
-//      }
-//    }
-//  })
-//})
-//
-//tail.stderr.on('data', function (data) {
-//  console.log('tail stderr: ' + data)
-//})
-//
-//tail.on('exit', function (code) {
-//  if (code !== 0) {
-//    console.log('tail process exited with code ' + code);
-//  }
-//})
 
 // functions
 
@@ -274,69 +211,4 @@ var broadcast = function(msg) {
   for (var id in connected) {
     connected[id].send(json)
   }
-}
-
-var htmlspecialchars = function (string, quote_style, charset, double_encode) {
-  // http://kevin.vanzonneveld.net
-  // +   original by: Mirek Slugen
-  // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +   bugfixed by: Nathan
-  // +   bugfixed by: Arno
-  // +    revised by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // +      input by: Ratheous
-  // +      input by: Mailfaker (http://www.weedem.fr/)
-  // +      reimplemented by: Brett Zamir (http://brett-zamir.me)
-  // +      input by: felix
-  // +    bugfixed by: Brett Zamir (http://brett-zamir.me)
-  // %        note 1: charset argument not supported
-  // *     example 1: htmlspecialchars("<a href='test'>Test</a>", 'ENT_QUOTES');
-  // *     returns 1: '&lt;a href=&#039;test&#039;&gt;Test&lt;/a&gt;'
-  // *     example 2: htmlspecialchars("ab\"c'd", ['ENT_NOQUOTES', 'ENT_QUOTES']);
-  // *     returns 2: 'ab"c&#039;d'
-  // *     example 3: htmlspecialchars("my "&entity;" is still here", null, null, false);
-  // *     returns 3: 'my &quot;&entity;&quot; is still here'
-
-  var optTemp = 0, i = 0, noquotes= false;
-  if (typeof quote_style === 'undefined' || quote_style === null) {
-      quote_style = 2;
-  }
-  string = string.toString();
-  if (double_encode !== false) { // Put this first to avoid double-encoding
-      string = string.replace(/&/g, '&amp;');
-  }
-  string = string.replace(/</g, '&lt;').replace(/>/g, '&gt;');
-
-  var OPTS = {
-      'ENT_NOQUOTES': 0,
-      'ENT_HTML_QUOTE_SINGLE' : 1,
-      'ENT_HTML_QUOTE_DOUBLE' : 2,
-      'ENT_COMPAT': 2,
-      'ENT_QUOTES': 3,
-      'ENT_IGNORE' : 4
-  };
-  if (quote_style === 0) {
-      noquotes = true;
-  }
-  if (typeof quote_style !== 'number') { // Allow for a single string or an array of string flags
-      quote_style = [].concat(quote_style);
-      for (i=0; i < quote_style.length; i++) {
-          // Resolve string input to bitwise e.g. 'PATHINFO_EXTENSION' becomes 4
-          if (OPTS[quote_style[i]] === 0) {
-              noquotes = true;
-          }
-          else if (OPTS[quote_style[i]]) {
-              optTemp = optTemp | OPTS[quote_style[i]];
-          }
-      }
-      quote_style = optTemp;
-  }
-  if (quote_style & OPTS.ENT_HTML_QUOTE_SINGLE) {
-      string = string.replace(/'/g, '&#039;');
-  }
-  if (!noquotes) {
-      string = string.replace(/"/g, '&quot;');
-  }
-
-  return string;
 }
